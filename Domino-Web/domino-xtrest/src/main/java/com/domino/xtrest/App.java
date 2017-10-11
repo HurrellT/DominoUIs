6599132b13b62286af27b2a875d3c95d4285f0da
lhazuca <lhazuca@gmail.com>
2017-10-11 00:26:57 -0300

    Primer commit para xtrest

---
+++ Domino-Web/domino-xtrest/src/main/java/com/domino/xtrest/App.java
@@ -0,0 +1,13 @@
+package com.domino.xtrest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
