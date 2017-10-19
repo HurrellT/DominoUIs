1feb02294f01846bb4ba38cfba1bb4ee03360f5a
lhazuca <lhazuca@gmail.com>
2017-10-19 18:07:12 -0300

    Agrego funcionalidad de agregar ingredientes a una pizza Ahora aparecen los ingredientes de la pizza sin poder modificar su ditribucion y solo modificamos los que agregamos desde la interfaz
    	modified:   Domino-Web/client/addIngredients/addIngredientesApp.js
    	modified:   Domino-Web/client/addIngredients/addIngredients.html

--- Domino-Web/client/addIngredients/addIngredientesApp.js
+++ Domino-Web/client/addIngredients/addIngredientesApp.js
@@ -8,19 +8,19 @@ app.controller("AddIngredienteCtrl" , function($scope) {
 		"id": 1,
 		"nombre": "Jamon",
 		"precio": 15,
-		"distribucionElegida": "Derecha"
+		"distribucionElegida": "Mitad derecha"
 	},
 	{
 		"id": 2,
 		"nombre": "Queso",
 		"precio": 20,
-		"distribucionElegida": "Izquierda"
+		"distribucionElegida": "Mitad Izquierda"
 	},
 	{
 		"id": 3,
 		"nombre": "Anchoas",
 		"precio": 50,
-		"distribucionElegida": "Derecha"
+		"distribucionElegida": "Mitad derecha"
 	},
 	{
 		"id": 4,
@@ -36,8 +36,35 @@ app.controller("AddIngredienteCtrl" , function($scope) {
 	}
 	];
 
-	$scope.distros = [ "Mitad Derecha",
+	this.agregar = function(ingred){
+		console.log(ingred);
+		$scope.promo.extras.push(ingred);
+	};
+
+	$scope.distros = [ "Mitad derecha",
 	"Toda la pizza",
 	"Mitad Izquierda"
 	];
+
+	$scope.promo =    
+	{
+		"id": 1,
+		"nombre": "Muzzarella",
+		"precio": 125,
+		"ingredientes": [
+		{
+			"id": 2,
+			"nombre": "Queso",
+			"precio": 20,
+			"distribucionElegida": "Mitad Izquierda"
+		},
+		{
+			"id": 1,
+			"nombre": "Jamon",
+			"precio": 15,
+			"distribucionElegida": "Mitad derecha"
+		}
+		],
+		"extras":[]
+	};
 });
