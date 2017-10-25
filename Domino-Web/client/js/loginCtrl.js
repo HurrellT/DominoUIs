b071ca1c87107af0e727fefb8f6edaa7f5e2bfb8
BrianLeoGoldman <leonel89011@gmail.com>
2017-10-25 01:53:11 -0300

    Arreglamos el muestreo del nombre del usuario

--- Domino-Web/client/js/loginCtrl.js
+++ Domino-Web/client/js/loginCtrl.js
@@ -5,12 +5,16 @@ dominoPizzaApp.controller('LoginCtrl', function ($rootScope, $state, UsuarioServ
 
     this.acceder = function() {
     	if(UsuarioService.hasUser(this.nombre, this.password)){
+			sessionStorage.setItem("Nombre", this.nombre);
     		$rootScope.usuario = UsuarioService.getUsuarioByName(this.nombre); //Will be validateLogin from Server
     		$state.go("crearPedido");
     	}else{
     		window.alert("Nombre de usuario o contraseña incorrectos");
     	}
         //Aca hay que hacer un request al server con los datos del login
-    };
+	};
+	
+
+	
     
 });
