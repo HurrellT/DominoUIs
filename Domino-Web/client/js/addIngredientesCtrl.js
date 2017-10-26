24fbb9e0fa83b1858827e37b49b19738a3ff705e
lhazuca <lhazuca@gmail.com>
2017-10-26 20:52:39 -0300

    Los extras en la pagina de agregar ingredientes se van eliminando si ya estan en la pizza

--- Domino-Web/client/js/addIngredientesCtrl.js
+++ Domino-Web/client/js/addIngredientesCtrl.js
@@ -1,25 +1,26 @@
-dominoPizzaApp.controller("AddIngredienteCtrl", function ($state,
-                                                          $stateParams,
-                                                          DistribucionesService,
-                                                          IngredienteService,
-                                                          PizzaService,
-                                                          TamanioService,
-                                                          PedidoService) {
+dominoPizzaApp.controller("AddIngredienteCtrl", function ($state,$stateParams,  DistribucionesService,  IngredienteService,  PizzaService,  TamanioService,  PedidoService) {
 
-    this.pizza = PizzaService.getPizzaById($stateParams.idP);
-    this.ingredientes = IngredienteService.ingredientes;
-    this.distros = DistribucionesService.distribuciones;
-    this.tamanio = TamanioService.getTamanioById($stateParams.idT);
+  this.pizza = PizzaService.getPizzaById($stateParams.idP);
+  var ingaux = this.pizza.ingredientes.map(function(i){return i.id;}).concat(this.pizza.extras.map(function(i){return i.id;}));
+  this.ingredientesDisponibles = IngredienteService.ingredientes.filter(yaEstaEnLaPizza);
 
-    var nombre = sessionStorage.getItem("Nombre");
-	document.getElementById("userName").innerHTML = nombre;
+  this.distros = DistribucionesService.distribuciones;
+  this.tamanio = TamanioService.getTamanioById($stateParams.idT);
 
-    this.agregar = function (ingred) {
-        this.pizza.extras.push(ingred);
-    };
 
-    this.generarPedido = function () {
-        // window.alert("En proceso");
-        $state.go("confirmarPedido({idP:this.pizza.id, idT:this.tamanio.id})")
-    };
+  var nombre = sessionStorage.getItem("Nombre");
+  document.getElementById("userName").innerHTML = nombre;
+
+  this.agregar = function (ingred) {
+    this.pizza.extras.push(ingred);
+  };
+
+  this.generarPedido = function () {
+    $state.go("confirmarPedido({idP:this.pizza.id, idT:this.tamanio.id})");
+  };
+
+  function yaEstaEnLaPizza (ingrediente){
+    console.log(ingaux);
+    return !_.includes(ingaux,ingrediente.id);
+  }
 });
